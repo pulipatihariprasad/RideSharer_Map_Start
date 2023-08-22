4f8d6226a3e4b077de9d4d2f5428af01262da447
Hariprasad <pulipati.hp@gmail.com>
2023-08-22 17:22:15 +0530

    Initial Commit

---
+++ RideSharer_Map_Start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RideSharer_Map_Start
+//
+//  Created by HariprasadPulipati on 22/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
